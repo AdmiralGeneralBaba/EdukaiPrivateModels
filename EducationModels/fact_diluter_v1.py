bef684281699acae0857b6717b3d663024434374
AdmiralGeneralBaba <davidtiareh2@gmail.com>
2023-11-12 20:01:11 +0000

    Created the 'fact_diluter_v1' module
    - Calls a agent, has it dilute a list of facts, then a bunch of regexing methods removes it.
    - need to create a holistic method.

--- EducationModels/fact_diluter_v1.py
+++ EducationModels/fact_diluter_v1.py
@@ -1,7 +1,9 @@
 from openai_calls import OpenAI
+from info_extractor_v4 import InfoExtractorV4
 import re
 
 
+#Chain for the fact diluter 
 def fact_diluter_chain(facts) : 
     llm = OpenAI()
     prompt = """You are to be an expert fact dilutor, tasked with reviewing a list of facts and only choosing the ones that are strictly relevant to a student studying the pure content of a book on a specified topic.
@@ -32,20 +34,37 @@ Review the following facts and excise any that meet the exclusion criteria promp
 
 
 #This extracts the facts to remove from the LLM output, and puts it in a string array where i=fact number to remove.
-def regex_facts_to_remove(facts_to_dilute) : 
-    import re
-
+def regex_facts_to_remove(facts) : 
     # The regex pattern to extract numbers within curly brackets
     pattern = r'\{(\d+)\}'
 
+    # Example input string
+    input_string = "{2},{3},{7},{9},{15}"
+
     # Using regex to find all matches
-    matches = re.findall(pattern, facts_to_dilute) 
+    matches = re.findall(pattern, input_string)
 
-    # Formatting the extracted numbers into the desired string array format
-    formatted_array = [f"'{{{num}}}'" for num in matches]
-    return formatted_array
+    # Formatting the extracted numbers into a string array without quotes
+    formatted_array = [num for num in matches]
 
-def remove_facts_from_input(facts_to_dilute) : 
-    pattern = 
-    for i in range(len(facts_to_dilute)) : 
-    
+    formatted_array
+
+
+#This removes the fact from the input text given a number
+def remove_fact(input_text, fact_number):
+    # Adjusted regex pattern to match the fact with the given number correctly
+    # Using a dot after the number to ensure accurate matching
+    pattern = rf'(?<!\d){fact_number}\.\s*\{{.*?\}}(?!\d)'
+
+    # Remove the matched fact
+    modified_text = re.sub(pattern, '', input_text)
+
+    return modified_text
+
+
+#this removes a list of numbers from a inputted text.
+def remove_facts_from_input(facts, array_to_remove) : 
+    for num in array_to_remove :
+        facts = remove_fact(facts, num)
+    return facts
+

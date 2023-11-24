d1d853c937b17bf6cc99c249bebf4d2eeaaf3245
AdmiralGeneralBaba <davidtiareh2@gmail.com>
2023-11-24 21:52:09 +0000

    small change

--- EducationModels/PowerpointCreator/ModulesCode/general_content_page/general_content_easy_bullet_points.py
+++ EducationModels/PowerpointCreator/ModulesCode/general_content_page/general_content_easy_bullet_points.py
@@ -7,27 +7,27 @@ from regexing_code import extract_content_TITLE_CONTENT_PICTURE
 def create_input_prompt(powerpoint_plan, slide_number) : 
     prompt = """I want you to pretend to be an expert teacher, making a perfectly constructed powerpoint slide for your students, so that it is easily readable. Based on this lesson description and the inputted facts, you are to create a SINGLE powerpoint slide  based on the facts given. provided will also be the powerpoint plan for context. 
 
-Start with a UNIQUE, INTERESTING title, by doing 
-TITLE {INSERT TITLE HERE}, CONTENT {INSERT THE CONTENT HERE}, followed by a search term for a picture to go along the facts as best you can, PICTURE {INSERT PICTURE QUERY HERE}. 
+    Start with a UNIQUE, INTERESTING title, by doing 
+    TITLE {INSERT TITLE HERE}, CONTENT {INSERT THE CONTENT HERE}, followed by a search term for a picture to go along the facts as best you can, PICTURE {INSERT PICTURE QUERY HERE}. 
 
-Tips for content:
-- Take into account the context of the overall lesson. 
-- Use these facts ONLY- DO NOT make up information/facts
-- DO NOT leave any space for placeholder (e.g for an image) - the powerpoint must be a finished product.
-- State the facts in a way that is easy for them to understand, but is not long-winded. 
+    Tips for content:
+    - Take into account the context of the overall lesson. 
+    - Use these facts ONLY- DO NOT make up information/facts
+    - DO NOT leave any space for placeholder (e.g for an image) - the powerpoint must be a finished product.
+    - State the facts in a way that is easy for them to understand, but is not long-winded. 
 
-An example output would be : 
-TITLE {title text here, with the curly brackets}, 
-CONTENT {content text here, with the curly brackets}, 
-PICTURE {picture text here, with the curly brackets}
+    An example output would be : 
+    TITLE {title text here, with the curly brackets}, 
+    CONTENT {content text here, with the curly brackets}, 
+    PICTURE {picture text here, with the curly brackets}
 
-Here is the powerpoint plan : 
+    Here is the powerpoint plan : 
 
-""" + "{ " f"{powerpoint_plan}" + "}" + """ 
+    """ + "{ " f"{powerpoint_plan}" + "}" + """ 
 
-The current slide number is """ + "{ " f"{slide_number}" + "}" + """ 
+    The current slide number is """ + "{ " f"{slide_number}" + "}" + """ 
 
-and here are the facts : """
+    and here are the facts : """
     return prompt
 
 
